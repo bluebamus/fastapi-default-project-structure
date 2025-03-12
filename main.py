da09797eff41c490d0414280a59b07b1e5a0612a
bluebamus <bluebamus@naver.com>
2025-03-12 22:40:38 +0900

    update

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from fastapi import FastAPI
 from core.settings import ENV_PATH, settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION,
+    description=settings.DESCRIPTION,
+)
