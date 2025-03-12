0fc1fcd73af155e5dfc898db543f432e70b59e4c
bluebamus <bluebamus@naver.com>
2025-03-12 22:40:38 +0900

    update

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,24 +1,31 @@
 """.env 환경설정 값을 관리합니다."""
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from lib.print_log import PrintLog
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional, List
+from pydantic import field_validator
 
 ENV_PATH = ".env"
 
 
 class Settings(BaseSettings):
     """.env 파일 설정 모델"""
+
     # .env 파일을 읽어서 환경변수를 설정합니다.
     model_config = SettingsConfigDict(
         env_file=ENV_PATH,
-        env_file_encoding='utf-8',
-        extra='ignore',  # extra=forbid (default)
+        env_file_encoding="utf-8",
+        extra="ignore",  # extra=forbid (default)
+        case_sensitive=True,
     )
 
-    IS_DEBUG: bool = False  # 디버그 모드
+    PROJECT_NAME: str = ""
+    VERSION: str = ""
+    DESCRIPTION: str = ""
+
+    DEBUG: bool = False  # 디버그 모드
 
     # 데이터베이스 설정
-    DB_TABLE_PREFIX: str = "g6_"
+    DB_TABLE_PREFIX: str = ""
     DB_ENGINE: str = ""
     DB_USER: str = ""
     DB_PASSWORD: str = ""
@@ -26,6 +33,7 @@ class Settings(BaseSettings):
     DB_PORT: int = 3306
     DB_NAME: str = ""
     DB_CHARSET: str = "utf8mb4"
+    DATABASE_URL: str = "sqlite:///./test.db"
 
     SESSION_COOKIE_NAME: str = "session"  # 세션 쿠키 이름
     SESSION_SECRET_KEY: str = ""  # 세션 비밀키
@@ -36,7 +44,7 @@ class Settings(BaseSettings):
     SMTP_USERNAME: str = ""
     SMTP_PASSWORD: str = ""
 
-    TIME_ZONE: str = "Asia/Seoul"  # 시간대
+    TIME_ZONE: str = ""  # 시간대
 
     # 에디터 업로드 설정
     UPLOAD_IMAGE_RESIZE: bool = False  # 에디터 이미지 리사이즈 사용
@@ -49,7 +57,15 @@ class Settings(BaseSettings):
     CORS_ALLOW_ORIGINS: str = "*"
     CORS_ALLOW_CREDENTIALS: bool = False
     CORS_ALLOW_METHODS: str = "*"
-    CORS_ALLOW_HEADERS:str = "*"
+    CORS_ALLOW_HEADERS: str = "*"
+
+    # 유효성 검사 예시
+    @field_validator("DATABASE_URL")
+    def assemble_db_url(cls, v: Optional[str], values: dict) -> str:
+        if v:
+            return v
+        return f"postgresql://{values.get('DB_USER')}:{values.get('DB_PASSWORD')}@{values.get('DB_HOST')}:{values.get('DB_PORT')}/{values.get('DB_NAME')}"
+
 
 settings = Settings()
 
@@ -75,7 +91,9 @@ class CORSConfig:
         if not isinstance(allow_credentials, bool):
             allow_credentials = settings.CORS_ALLOW_CREDENTIALS.lower() == "true"
         if allow_credentials and self.allow_origins == ["*"]:
-            print("allow_origins를 모두 허용하는 것과 allow_credentials를 True로 함께 설정할 수 없습니다.")
+            print(
+                "allow_origins를 모두 허용하는 것과 allow_credentials를 True로 함께 설정할 수 없습니다."
+            )
             print("보안을 위해 allow_credentials를 False로 설정합니다.")
             allow_credentials = False
         return allow_credentials
